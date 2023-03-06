f55d99779bcb31dadf583cf55146a8322cb858db
ricardo <ricardo@in8.com.br>
2023-03-06 20:26:14 -0300

    Obtendo um usuário por id

--- src/main/java/com/whorkshop/mongo/service/UserService.java
+++ src/main/java/com/whorkshop/mongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.whorkshop.mongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.whorkshop.mongo.domain.User;
 import com.whorkshop.mongo.repository.UserRepository;
+import com.whorkshop.mongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
