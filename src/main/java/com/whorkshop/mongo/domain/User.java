b5763a8173b9b37e47355122d43e4700016f9120
ricardo <ricardo@in8.com.br>
2023-03-08 19:56:55 -0300

    Referenciando os posts do usuário

--- src/main/java/com/whorkshop/mongo/domain/User.java
+++ src/main/java/com/whorkshop/mongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.whorkshop.mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -15,7 +18,8 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	
-//	private List<Post> posts;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {}
 
@@ -48,6 +52,14 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
