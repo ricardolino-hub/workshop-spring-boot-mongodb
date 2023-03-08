971cd548292b92b01ad40b2a6b11715ea285ad63
ricardo <ricardo@in8.com.br>
2023-03-07 21:00:42 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/whorkshop/mongo/config/Instantiation.java
+++ src/main/java/com/whorkshop/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.whorkshop.mongo.domain.Post;
 import com.whorkshop.mongo.domain.User;
+import com.whorkshop.mongo.dto.AuthorDTO;
 import com.whorkshop.mongo.repository.PostRepository;
 import com.whorkshop.mongo.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
-		postReposiroty.saveAll(Arrays.asList(post1, post2));	
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 }
 
