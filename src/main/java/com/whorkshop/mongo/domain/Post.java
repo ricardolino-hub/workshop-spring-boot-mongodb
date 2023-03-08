6cfdfdf184fcdd098eb4050d8dd8d3786a9f82b1
ricardo <ricardo@in8.com.br>
2023-03-08 20:36:02 -0300

    Obtendo um post por id e Acrescentando comentários aos posts

--- src/main/java/com/whorkshop/mongo/domain/Post.java
+++ src/main/java/com/whorkshop/mongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.whorkshop.mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.whorkshop.mongo.dto.AuthorDTO;
+import com.whorkshop.mongo.dto.CommentDTO;
 
 @Document(collation = "post")
 public class Post implements Serializable{
@@ -18,7 +21,10 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
+	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +76,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
---
+++ src/main/java/com/whorkshop/mongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.whorkshop.mongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.whorkshop.mongo.domain.Post;
+import com.whorkshop.mongo.repository.PostRepository;
+import com.whorkshop.mongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
