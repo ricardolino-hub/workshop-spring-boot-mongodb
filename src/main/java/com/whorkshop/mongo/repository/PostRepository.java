973db5d8967392c09d82959c5535442bacab8c85
ricardo <ricardo@in8.com.br>
2023-03-13 16:36:58 -0300

    Consulta simples com @Query

--- src/main/java/com/whorkshop/mongo/repository/PostRepository.java
+++ src/main/java/com/whorkshop/mongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.whorkshop.mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.whorkshop.mongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
